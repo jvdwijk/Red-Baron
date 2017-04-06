43513d05de87949536d81a6c3a6388d6b9afdd9c
jvdwijk <joeyvdwijk@gmail.com>
2017-04-06 11:28:46 +0200

    AirplaneFuel

---
+++ Mythe/Assets/Scripts/GameObjects/Airplane/AirplaneFuel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GameObjects.Airplane
+{
+    public class AirplaneFuel : MonoBehaviour
+    {
+
+        [SerializeField]
+        private int _fuelAmount = 120;
+        [SerializeField]
+        private int _fuelDrop = 1;
+
+        private void Start()
+        {
+            StartCoroutine("FuelUsage");
+        }
+
+        private IEnumerator FuelUsage()
+        {
+            while (_fuelAmount > 0)
+            {
+                _fuelAmount -= _fuelDrop;
+                yield return new WaitForSeconds(1);
+            }
+            if (_fuelAmount == 0)
+            {
+                //Function dying
+            }
+        }
+    }
+}
