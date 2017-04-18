30279366938c8ee0fbe82693125552a3efa596a1
jvdwijk <joeyvdwijk@gmail.com>
2017-04-18 11:41:12 +0200

    enemy ai functions, summaries

--- Mythe/Assets/Scripts/Enemy/Attack.cs
+++ Mythe/Assets/Scripts/Enemy/Attack.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.VR.WSA.WebCam;
-
+﻿using UnityEngine;
 
 public class Attack : State
 {
     [SerializeField] private Transform _target;
     [SerializeField] private int _distanceToTarget = 20;
     [SerializeField] private GameObject _muzzle;
-    [SerializeField] private int _delay;
-    [SerializeField] private int _waitTime;
     private Turret _turret;
     private EnemyMovement _enemyMovement;
 
@@ -22,19 +16,13 @@ public class Attack : State
 
     public override void Action()
     {
-        _enemyMovement.Move();
-        if (_waitTime <= Time.realtimeSinceStartup)
-        {
-            _turret.Shoot(_muzzle);
-            _waitTime = (int)Time.realtimeSinceStartup + _delay;
-        }
+        _enemyMovement.Move(_target.position);
+        _turret.Shoot(_muzzle);
     }
 
     public override void Reaction()
     {
-        if (Vector3.Distance(transform.position, _target.position) >= _distanceToTarget)
-        {
+        if (Vector3.Distance(transform.position, _target.position) < _distanceToTarget) return;
             GetComponent<StateMachine>().SetState( StateId.WanderState );
-        }
     }
 }
