26079a5edde7a8cc7f0bc5716e22eaa9be935a67
Casper Heringa <casper@MacBook-Pro-van-Casper.local>
2017-05-22 08:54:52 +0200

    Fading update

---
+++ Mythe/Assets/Scripts/Missions/SceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	private float _counter;
+	[SerializeField]
+	private float _switchDelay;
+
+	void Start(){
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag("Player")) {
+			//float fadeTime = GameObject.Find ("MissionCollider").GetComponent<Fading> ().BeginFade (1);
+			EditorSceneManager.LoadScene("SceneTesting");
+			}
+		}
+	}
+
+
+	
+}
